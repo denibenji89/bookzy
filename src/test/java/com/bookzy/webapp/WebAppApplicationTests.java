b9898ab5399af84426799acee6f8c093c9fd4540
denibenji89 <benjichan89@hotmail.com>
2021-06-27 03:40:33 -0700

    Add jenkin test

--- src/test/java/com/bookzy/webapp/WebAppApplicationTests.java
+++ src/test/java/com/bookzy/webapp/WebAppApplicationTests.java
@@ -1,13 +1,17 @@
 package com.bookzy.webapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void simpleTest() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a+b==3);
 	}
 
 }
