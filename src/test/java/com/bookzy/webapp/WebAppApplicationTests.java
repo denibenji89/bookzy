228c0d005bb46dc09ea75c74829365dd56f008be
denibenji89 <benjichan89@hotmail.com>
2021-06-30 20:27:30 -0700

    jubnit add

--- src/test/java/com/bookzy/webapp/WebAppApplicationTests.java
+++ src/test/java/com/bookzy/webapp/WebAppApplicationTests.java
@@ -3,15 +3,26 @@ package com.bookzy.webapp;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.*;
 @SpringBootTest
 class WebAppApplicationTests {
 
 	@Test
-	public void simpleTest() {
+	public void test1() {
 		int a = 1;
 		int b = 2;
 		assertTrue(a+b==3);
 	}
-
+	@Test
+	public void test2() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a+b==3);
+	}
+	@Test
+	public void test3() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a+b==3);
+	}
 }
